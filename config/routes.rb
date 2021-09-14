d2dc827e7db79150284f67f02767cdf06c5186ae
harsh291199 <harshsuthar2911@gmail.com>
2021-09-14 12:42:46 +0530

    Create Simple project and add functionality of Rails internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :feedbacks
+    root 'static_pages#index'
+  end
 end
